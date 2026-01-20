b438d8096b1592503ed38be6d79f9e60edb8da4e
AzureAD\JoelRocaMolano <joelrocamolano@gmail.com>
2026-01-20 12:29:50 +0100

    aa

--- src/app/features/admin/maintenance-worker/maintenance-worker.ts
+++ src/app/features/admin/maintenance-worker/maintenance-worker.ts
@@ -295,7 +295,6 @@ export class MaintenanceWorker implements OnInit {
       });
   }
 
-  
   skipUserCreation() {
       this.userDialog = false;
       this.messageService.add({ severity: 'info', summary: 'Info', detail: 'Trabajador creado sin usuario asignado' });
